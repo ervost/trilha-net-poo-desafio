1a08ae6e5573d15a1ca4ac4f3c8d8f97636680f8
Airton <ervost@gmail.com>
2022-09-14 08:22:07 -0300

    Solução do desafio proposto.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone iPhone:");
+Iphone iphone = new Iphone(numero: "1234", modelo: "Iphone XS", imei: "123123123", memoria: 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Chrome");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Nokia:");
+Nokia nokia = new Nokia(numero: "5678", modelo: "Nokia SX", imei: "789987654", memoria: 128);
+nokia.ReceberLigacao();
+nokia.Ligar();
+nokia.InstalarAplicativo("Safari");
